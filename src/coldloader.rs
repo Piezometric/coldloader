5138da9cf8cfa151579ac98ebf23488152f1b696
Piezometric <ugurxkahriman@gmail.com>
2026-07-24 19:32:06 +0300

    Refactor ColdLoader functions and remove DllMain

--- src/coldloader.rs
+++ src/coldloader.rs
@@ -1,53 +1,9 @@
-mod logging;
-mod ini;
-
 use std::{ffi::OsString, os::windows::ffi::OsStringExt, path::{Path, PathBuf}, sync::{LazyLock, OnceLock}};
-
 use anyhow::{anyhow, Result};
-use ini::CONFIG;
 use winapi::{shared::minwindef::{BOOL, DWORD, HMODULE, LPVOID, TRUE}, um::{libloaderapi::{GetModuleFileNameW, LoadLibraryW}, processthreadsapi::GetCurrentProcessId, winnt::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH}}};
 
+use crate::ini::CONFIG;
 use crate::logging::message_box;
-
-static DLL_PATH: OnceLock<PathBuf> = OnceLock::new();
-
-#[unsafe(no_mangle)]
-pub extern "system" fn DllMain(module: HMODULE, reason: DWORD, _reserved: LPVOID) -> BOOL {
-    match reason {
-        DLL_PROCESS_ATTACH => {
-            let dll_path = unsafe {
-                let mut buffer = [0u16; 1024];
-                let len = GetModuleFileNameW(module, buffer.as_mut_ptr(), buffer.len() as u32);
-                let binding = OsString::from_wide(&buffer[..len as usize]).into_string().unwrap();
-                let path = Path::new(binding.as_str());
-                path.parent().unwrap().to_path_buf()
-            };
-            DLL_PATH.set(dll_path).unwrap();
-
-            logging::init_logger();
-            logging::setup_panic_handler();
-
-            initialize().unwrap_or_else(|e| {
-                log::error!("Failed to initialize: {}", e);
-                message_box(format!("Failed to initialize ColdLoader:\n{}", e).as_str());
-                std::process::exit(1);
-            });
-
-            std::thread::spawn(|| {
-                std::thread::sleep(std::time::Duration::from_secs(CONFIG.cleanup_delay));
-                log::info!("Performing cleanup in background thread");
-                let _ = cleanup();
-            });
-        }
-        DLL_PROCESS_DETACH => {
-            log::info!("DLL_PROCESS_DETACH called, performing cleanup");
-            let _ = cleanup();
-        }
-        _ => {}
-    }
-    
-    TRUE
-}
 
 static STEAM_UNIVERSE: &str = "public";
 static PROCESS_ID: LazyLock<u32> = LazyLock::new(|| unsafe {
@@ -108,13 +64,11 @@ fn set_steam_env_vars(app_id: u32) {
     log::info!("Set Steam environment variables");
 }
 
-fn initialize() -> Result<()> {
+pub fn initialize() -> Result<()> {
     set_steam_env_vars(CONFIG.app_id);
 
-    // Patch the registry
     patch_registry()?;
     
-    // Load the DLLs
     let client_path = CONFIG.steamclient_path.to_string_lossy().to_string();
     log::info!("Loading steamclient from path: {}", client_path);
     let client_dll = unsafe {
@@ -151,7 +105,7 @@ fn initialize() -> Result<()> {
     Ok(())
 }
 
-fn cleanup() -> Result<()> {
+pub fn cleanup() -> Result<()> {
     let install_path = winreg::RegKey::predef(winreg::enums::HKEY_LOCAL_MACHINE)
         .open_subkey_with_flags("Software\\WOW6432Node\\Valve\\Steam", winreg::enums::KEY_ALL_ACCESS)
         .and_then(|key| {
